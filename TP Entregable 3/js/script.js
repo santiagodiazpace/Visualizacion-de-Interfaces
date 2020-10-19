7dd325db5e7c341b928a84fb6ee89304133063dc
Santiago Diaz Pace <sdiazpace@gmail.com>
2020-10-18 23:44:46 -0300

    Agregado de funcionalidad

--- TP Entregable 3/js/script.js
+++ TP Entregable 3/js/script.js
@@ -1,5 +1,6 @@
 
-const DATE_TARGET = new Date('11/01/2020 0:01 AM');
+const DATE_TARGET = new Date('11/10/2020 0:01 AM');                  // Fecha avant premier
+
 const SPAN_DAYS = document.querySelector('span#days');
 const SPAN_HOURS = document.querySelector('span#hours');
 const SPAN_MINUTES = document.querySelector('span#minutes');
@@ -10,8 +11,7 @@ const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
 const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
 const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24
 
-function updateCountdown() {
-    
+function updateCountdown() {  
     const NOW = new Date()
     const DURATION = DATE_TARGET - NOW;
     const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
@@ -19,10 +19,10 @@ function updateCountdown() {
     const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
     const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
 
-    SPAN_DAYS.textContent = REMAINING_DAYS;
-    SPAN_HOURS.textContent = REMAINING_HOURS;
-    SPAN_MINUTES.textContent = REMAINING_MINUTES;
-    SPAN_SECONDS.textContent = REMAINING_SECONDS;
+    SPAN_DAYS.innerHTML = REMAINING_DAYS;
+    SPAN_HOURS.innerHTML = REMAINING_HOURS;
+    SPAN_MINUTES.innerHTML = REMAINING_MINUTES;
+    SPAN_SECONDS.innerHTML = REMAINING_SECONDS;
 }
 
 updateCountdown();
